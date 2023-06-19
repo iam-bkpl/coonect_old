948142b82e8f69a8cef690e27dea8fa7cf0c70d0
iam-bkpl <kapil.bhandari@apexcollege.edu.np>
2023-06-19 23:33:59 +0545

    add images

--- backend/core/context_processors.py
+++ backend/core/context_processors.py
@@ -1,20 +1,20 @@
-from core.models import Contact, Service, Timing
+from core.models import Contact, Event, Service, Timing
 
 # In your Django project directory
 # Create a new file called "context_processors.py"
+
 
 def navbar_footer_context(request):
     # Retrieve the necessary objects
     contact_data = Contact.objects.last()
     service_data = Service.objects.all()[:4]
     timing_data = Timing.objects.last()
-
+    event_date = Event.objects.all()
 
     # Return a dictionary with the objects
     return {
-        'contact_data': contact_data,
-        'footer_data': service_data,
-        'timing_data':timing_data,
-        'service_data':service_data,
-        
+        "contact_data": contact_data,
+        "footer_data": service_data,
+        "timing_data": timing_data,
+        "service_data": service_data,
     }
